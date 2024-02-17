6f2f9f0c8988db9d2cb0b2962558b147a8bd2422
Yussuf <yussuf.saumu@gmail.com>
2024-02-17 15:06:18 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AdminLogInPage from "./components/AdminLoginPage";
-
-
 function App() {
-    return(
-      <Router>
-        <Routes>
-          <Route path="/adminlogin" element={<AdminLogInPage />} />
-        </Routes>
-      </Router>
-    )
   
 }
 
