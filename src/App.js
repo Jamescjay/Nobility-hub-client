011a20343e8bf0d8e447d3702230dc8eeee5f9f8
Yussuf <yussuf.saumu@gmail.com>
2024-02-17 18:58:36 +0300

    OAuth 2.0 credentials Set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
-import wideImage from "./wider-image.jpg"; 
+import wideImage from "./wider-image.jpg";
 
 function App() {
+  const handleSignIn = () => {
+    const clientId =
+      "903014081128-03ap17fghal5cugp545qt6uu315lf7e4.apps.googleusercontent.com";
+    const redirectUri = encodeURIComponent(window.location.origin);
+
+      window.location.href = `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&scope=email profile openid`;
+  };
+
   return (
     <div className="app-container">
       <div className="left-section">
@@ -24,7 +32,6 @@ function App() {
       </div>
       <div className="right-section">
         <p>
-          {" "}
           Already have an account?
           <a href="#" onClick={() => window.location.reload()}>
             Log in
@@ -33,7 +40,9 @@ function App() {
         <div className="cta-section">
           <div className="cta-tab">
             <h2>Connect, Collaborate, Learn</h2>
-            <button className="sign-in-button">Sign In with Nob Account</button>
+            <button className="sign-in-button" onClick={handleSignIn}>
+              Sign In with Nob Account
+            </button>
           </div>
         </div>
       </div>
