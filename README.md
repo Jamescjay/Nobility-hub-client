65666b7d77fe6fde3a5d8a6277a8534afaa4d3de
festus-sulumeti <sulumetifestus@outlook.com>
2024-02-19 17:15:55 +0300

    Project files

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 modules to install:
  1. npm install react-router-dom
- 
+ 2. npm install lucide-react
+ 3. npm install react-icons
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,31 @@
+// the homepage of the app
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./Home";
-import ParentLogin from "./ParentLogin"; // Assuming you have a ParentLogin component
-import RegularLogin from "./RegularLogin"; // Assuming you have a RegularLogin component
+import { Link } from "react-router-dom";
+import { ArrowRight } from "lucide-react";
+import "../styling/Home.css"// Import the custom CSS file
 
-const App = () => {
+const Home = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/parents/login" component={ParentLogin} />
-        <Route path="/login" component={RegularLogin} />
-        {/* Add other routes as needed */}
-      </Switch>
-    </Router>
+    <div className="home-container">
+      <h1 className="main-heading">
+        Nobility HUb {" "}
+        <span className="text-blue-600">worksession</span> innovating through technology.
+      </h1>
+      <p className="sub-text">
+      We firmly believe in granting everyone the chance to thrive in the technology and programming sector.
+      Through our groundbreaking educational methods, we equip students with the essential skills and expertise required to embark on prosperous career paths. 
+      Come join us and be a pioneer of tomorrow!
+      </p>
+      <div className="button-container">
+        <Link className="primary-button" to={"/adminlogin"}>
+        Admin's Login <ArrowRight className="arrow-icon" />
+        </Link>
+        <Link className="secondary-button" to={"/LearnersLogin"}>
+        Learner's Login <ArrowRight className="arrow-icon" />
+        </Link>
+      </div>
+    </div>
   );
 };
 
-export default App;
+export default Home;
