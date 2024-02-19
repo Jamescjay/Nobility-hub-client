0919ae2af240ce137b994340fa4e7673e7b6e8bd
festus-sulumeti <sulumetifestus@outlook.com>
2024-02-19 22:23:14 +0300

    Learners dashboard

---
+++ src/components/Learnersdashboard.js
@@ -0,0 +1,33 @@
+// src/components/Dashboard.js
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+// import LearnersDashboard from './Learnersdashboard';
+
+const LearnersDashboard = ({ user }) => {
+  const navigate = useNavigate();
+
+  // Check if user is defined before accessing properties
+  if (!user || !user.email || !user.role) {
+    return (
+      <div>
+        <p>Error: User data is not available.</p>
+        {/* <button onClick={() => navigate('/admiogin')}>Go to Login</button> */}
+      </div>
+    );
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    // navigate('/adminlogin');
+  };
+
+  return (
+    <div>
+      <h1>Welcome to the Dashboard, {user.email}!</h1>
+      <p>Role: {user.role}</p>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+};
+
+export default LearnersDashboard;
