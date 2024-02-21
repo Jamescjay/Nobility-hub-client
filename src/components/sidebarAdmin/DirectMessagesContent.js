efe0205310022986694041fc8d8115ea6eafdab4
festus-sulumeti <sulumetifestus@outlook.com>
2024-02-21 06:56:09 +0300

    The frontend side

---
+++ src/components/sidebarAdmin/DirectMessagesContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DirectMessagesContent = () => {
+  return (
+    <div>
+      {/* Content for Direct Messages section */}
+      <h2>Direct Messages Content</h2>
+      {/* Add your specific content here */}
+    </div>
+  );
+};
+
+export default DirectMessagesContent;
