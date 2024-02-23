dde145b16e22b60560f78f7f6a1bb6dc1bfd4b1d
emmanuel540! <emmanuelhillary24@gmail.com>
2024-02-23 20:15:44 +0300

    updates

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AdminLoginPage from "./components/AdminLoginPage";
+import LearnersLogin from "./components/LearnersLogin";
+import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
+import LearnersDashboard from "./components/Learnersdashboard";
+import Canvas from "./components/sidebarAdmin/Canvas";
+import CanvasHome from "./components/sidebarLearner/CanvasDashboard";
+import Overall from "./components/sidebarAdmin/Overall";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/adminlogin" element={<AdminLoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+
+        <Route path="/Canvas" element={<Canvas />} />
+        <Route path="/Overall" element={<Overall />} />
+
+        <Route path="/Learnersdashboard" element={<LearnersDashboard />} />
+        <Route path="/CanvasHome/*" element={<CanvasHome />} />
+        <Route path="/LearnersLogin" element={<LearnersLogin />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
