5d4297a9871f29ccc3dc4dbd7975072296f88963
Evie-Mututa <evemututa29@gmail.com>
2024-02-23 16:24:04 +0300

    Updated Messaging

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,46 +1,49 @@
 import React, { useState } from 'react';
-import NavigationBar from './NavigationBarAdmin';
+import NavigationBarAdmin from './NavigationBarAdmin';
 import SidebarAdmin from './SidebarAdmin';
 import MessagingSectionAdmin from './messaging/MessagingSectionAdmin';
- // Import Canvas component
 import '../styling/Dashboard.css';
 
+
 const Admindashboard = () => {
-  const [nobilityHubOpen, setNobilityHubOpen] = useState(false);
-  const [channelsOpen, setChannelsOpen] = useState(false);
-  const [studentsDropdownOpen, setStudentsDropdownOpen] = useState(false);
- 
-  const [selectedSection, setSelectedSection] = useState(null);
+ const [nobilityHubOpen, setNobilityHubOpen] = useState(false);
+ const [channelsOpen, setChannelsOpen] = useState(false);
+ const [studentsDropdownOpen, setStudentsDropdownOpen] = useState(false);
+ const [selectedSection, setSelectedSection] = useState(null);
 
-  const handleLogout = () => {
-    console.log('Logout clicked');
-    // Implement your logout logic here
-  };
 
-  // const handleSectionClick = (section) => {
-  //   setSelectedSection(section);
-  // };
+ const handleLogout = () => {
+   console.log('Logout clicked');
+   // Implement your logout logic here
+ };
 
-  const handleSectionClick = (section) => {
-  setSelectedSection(section);
-  };
 
-  return (
-    <div className="learners-dashboard-container">
-      <NavigationBar  handleLogout={handleLogout} />
-      <SidebarAdmin
-        handleSectionClick={handleSectionClick}
-        nobilityHubOpen={nobilityHubOpen}
-        setNobilityHubOpen={setNobilityHubOpen}
-        channelsOpen={channelsOpen}
-        setChannelsOpen={setChannelsOpen}
-        studentsDropdownOpen={studentsDropdownOpen}
-        setStudentsDropdownOpen={setStudentsDropdownOpen}
-        
-      />
-      <MessagingSectionAdmin />
-    </div>
-  );
+ const handleSectionClick = (section) => {
+   setSelectedSection(section);
+ };
+
+
+ return (
+   <div className="learners-dashboard-container">
+     <NavigationBarAdmin handleLogout={handleLogout} />
+     <div className="learners-main-content">
+       <SidebarAdmin
+         handleSectionClick={handleSectionClick}
+         nobilityHubOpen={nobilityHubOpen}
+         setNobilityHubOpen={setNobilityHubOpen}
+         channelsOpen={channelsOpen}
+         setChannelsOpen={setChannelsOpen}
+         studentsDropdownOpen={studentsDropdownOpen}
+         setStudentsDropdownOpen={setStudentsDropdownOpen}
+       />
+       <MessagingSectionAdmin />
+     </div>
+   </div>
+ );
 };
 
+
 export default Admindashboard;
+
+
+
