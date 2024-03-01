2d655d7cda21b7b20d80cc9d064af53b1655edf9
James <james.ekasiba@student.moringaschool.com>
2024-03-01 11:11:36 +0300

    message

--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -18,6 +18,7 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     localStorage.removeItem("userId");
     localStorage.removeItem("accessToken");
+    localStorage.removeItem("user");
     setUserId(null);
   };
 
